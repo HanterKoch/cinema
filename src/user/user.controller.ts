4c7d512be6bf20b1cae025dbadfa99e8bddec703
HanterKoch <94229658+HanterKoch@users.noreply.github.com>
2023-10-02 16:19:43 +0700

    Commit

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, HttpCode, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { User } from './decorators/user.decorator';
 import { UpdateUserDto } from 'src/auth/dto/update-user.dto';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
 
 @Controller('users')
 export class UserController {
@@ -27,7 +28,7 @@ export class UserController {
     @Put(':id')
     @HttpCode(200)
     @Auth('admin')
-    async updateUser(@User('_id') _id: string, @Body() dto: UpdateUserDto){
+    async updateUser(@Param('_id', IdValidationPipe) _id: string, @Body() dto: UpdateUserDto){
         return this.userService.updateProfile(_id, dto)
     }
 }
